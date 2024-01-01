6563cbceac0e147515e91f9e34e5e01bc46c9aa7
Teodorescu Iulia-Elena <93094532+AiluielenA@users.noreply.github.com>
2024-01-01 22:09:05 +0200

    figured out the bug

--- MilitaryDefenseApp/MilitaryDefenseApp/search/views.py
+++ MilitaryDefenseApp/MilitaryDefenseApp/search/views.py
@@ -13,28 +13,41 @@ QUERY_MAP = {
             dbr:United_States_Armed_Forces dbp:commander ?commander.
         }
     """,
+    'us_army_commander': """
+        SELECT ?name ?description WHERE {
+            dbr:Chief_of_Staff_of_the_United_States_Army dbp:name ?name .
+            OPTIONAL { dbr:Chief_of_Staff_of_the_United_States_Army rdfs:comment ?description FILTER (LANG(?description) = 'en') }
+        }
+    """,
 }
 
 def search(request):
-    query_type = request.GET.get("type", 'commanders')
+    search_term = request.GET.get("query", '').lower().strip()
+    query_type = ''
     
-    if query_type not in QUERY_MAP:
-        return HttpResponse("Invalid query type", status=400)
+    if 'commander' in search_term and 'us army' in search_term:
+        query_type = 'commanders'
+    elif 'commander' in search_term and 'us armed forces' in search_term:
+        query_type = 'us_army_commander'
     
-    search_term = request.GET.get("query", '').strip()
-    
-    if search_term:
-    
+    if query_type in QUERY_MAP:
         sparql = SPARQLWrapper("http://dbpedia.org/sparql")
         sparql.setReturnFormat(JSON)
-
-        sparql.setQuery(QUERY_MAP[query_type])
+        sparql_query = QUERY_MAP[query_type]
+        sparql.setQuery(sparql_query)
         results = sparql.query().convert()
         
-        data = [result["commander"]["value"] for result in results["results"]["bindings"]]
+        if query_type == 'us_army_commander':
+            data = [result["name"]["value"] for result in results["results"]["bindings"]]
+        else:
+            data = [result["commander"]["value"] for result in results["results"]["bindings"] if "commander" in result]
         
-        return render(request, 'search.html', {'data': data, 'query': search_term})
-    return render(request, 'search.html', {'message': 'No information found.'})
+        if data:
+                return render(request, 'search.html', {'data': data})
+        else:
+            return render(request, 'search.html', {'message': 'No information found.'})
+    else:
+        return render(request, 'search.html', {'message': 'Please specify your query more clearly. For example: "Who is the commander of the US Army?"'})
 
 
 # from django.http import HttpResponse, JsonResponse
