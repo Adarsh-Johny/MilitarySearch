94ce50a1b2c5f43e01bc87fb1868dc96b2c32978
adarshjohny <joad23dh@student.ju.se>
2024-01-03 15:13:44 +0100

    cors added and removed unwanted API

--- MilitaryDefenseApp/MilitaryDefenseApp/search/search_api.py
+++ MilitaryDefenseApp/MilitaryDefenseApp/search/search_api.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, jsonify
 from query_processor import execute_search
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins='*')  # Enable CORS for all origins
 
 @app.route('/api/search', methods=['POST'])
 def api():
     data = request.json
 
-    if "searchText" in data and "Country" in data and "isAuthorized" in data:
+    if "searchText" in data and "country" in data and "isAuthorized" in data:
         search_query = data["searchText"].lower().strip()
 
         query_result = execute_search(search_query)
 
         response_data = {
             "searchText": data["searchText"],
-            "Country": data["Country"],
+            "country": data["country"],
             "isAuthorized": data["isAuthorized"],
             "queryResult": query_result
         }
@@ -24,10 +26,5 @@ def api():
         error_response = {"error": "Missing required fields"}
         return jsonify(error_response), 400
 
-
-@app.route('/api/abc', methods=['GET'])
-def api2():
-    return jsonify("response_data")
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000)
