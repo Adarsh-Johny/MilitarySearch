e0b0f6d7919255641a45cba3721d678a15c8615c
adarshjohny <joad23dh@student.ju.se>
2024-01-11 02:55:21 +0100

    changes for API of user and miltary camps

--- MilitaryDefenseApp/db_api.py
+++ MilitaryDefenseApp/db_api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint , jsonify
-from db_models import MilitaryCampDbModel
+from db_models import MilitaryCampDbModel,UserModel
 
-db_api_blueprint = Blueprint('military_camps_api', __name__, url_prefix='')
+db_api_blueprint = Blueprint('db_api', __name__, url_prefix='')
 
 @db_api_blueprint.route('military-camps', methods=['GET'])
 def get_military_camps():
@@ -17,3 +17,18 @@ def get_military_camps():
         } for camp in military_camps]
     
     return jsonify(camps_list)
+
+
+@db_api_blueprint.route('users', methods=['GET'])
+def get_users():
+    users = UserModel.query.all()
+    users_list = [{
+        'userName': user.username,
+        'firstName': user.first_name,
+        'LastName': user.last_name,
+        'email': user.email,
+        'status': user.is_active,
+        'type': user.type,
+        } for user in users]
+    
+    return jsonify(users_list)
--- MilitaryDefenseApp/db_models.py
+++ MilitaryDefenseApp/db_models.py
@@ -11,3 +11,27 @@ class MilitaryCampDbModel(db.Model):
     executes_action = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(50), nullable=False)
     service_branch = db.Column(db.String(50), nullable=False)
+
+
+class UserModel(db.Model):
+    __tablename__ = 'users_user'
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    password = db.Column(db.String(128), nullable=False)
+    last_login = db.Column(db.DateTime, nullable=True)
+    is_superuser = db.Column(db.Boolean, default=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    first_name = db.Column(db.String(150), nullable=True)
+    last_name = db.Column(db.String(150), nullable=True)
+    email = db.Column(db.String(254), nullable=True)
+    is_staff = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=True)
+    date_joined = db.Column(db.DateTime, nullable=True)
+    type = db.Column(db.String(50), default='ADMIN', nullable=False)
+    rank = db.Column(db.String(50), nullable=True)
+    unit = db.Column(db.String(100), nullable=True)
+    gender = db.Column(db.String(50), nullable=True)
+    dateEnlisted = db.Column(db.String(50), nullable=True)
+    dateDischarged = db.Column(db.String(50), nullable=True)
+    previousTraining = db.Column(db.String(50), nullable=True)
+    awards = db.Column(db.String(50), nullable=True)
