5481fd58f4f8d9610f29bc4d2d9c653e99a6c90d
adarshjohny <joad23dh@student.ju.se>
2024-01-12 00:04:05 +0100

    changes for login

--- MilitaryDefenseApp/users/user_api.py
+++ MilitaryDefenseApp/users/user_api.py
@@ -1,32 +1,34 @@
 from flask import Blueprint, jsonify, request
-from flask_restful import Resource, Api
+from flask_restful import Api
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
-
 from django.contrib.auth import authenticate
+from django import setup as django_setup  # Import Django's setup function
+
+# Set the Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MilitaryDefenseApp.settings")
+
+# Call Django's setup function
+django_setup()
 
 user_blueprint = Blueprint("user", __name__)
 api = Api(user_blueprint)
 
-class AuthResource(Resource):
-    def post(self):
-        data = request.get_json()
+@user_blueprint.route('/login', methods=['POST'])
+def api_login():
+    data = request.json
 
-        username = data.get("username")
-        password = data.get("password")
+    username = data.get("username")
+    password = data.get("password")
 
-        if username is None or password is None:
-            response_data = {"error": "Missing required fields"}
-            return response_data, 400
+    if username is None or password is None:
+        response_data = {"error": "Missing required fields"}
+        return response_data, 400
 
-        user = authenticate(username=username, password=password)
+    user = authenticate(username=username, password=password)
 
-        if user is not None:
-            response_data = {"message": "Authentication successful", "role": user.type}
-            return jsonify(response_data)
-        else:
-            response_data = {"message": "Authentication failed"}
-            return response_data, 401
-
-# Define the API endpoint
-api.add_resource(AuthResource, "/login")
+    if user is not None:
+        response_data = {"message": "Authentication successful", "role": user.type}
+        return jsonify(response_data)
+    else:
+        response_data = {"message": "Authentication failed"}
+        return response_data, 401
